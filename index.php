c073a3e74bd7ffe99499adfe2d85f39e4e3ed978
greg-randall <gregrr@gmail.com>
2017-03-25 13:27:44 -0500

    added some debug lines & added a way to change the EOL charecter. "php_eol" worked on mac but not on windows

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <pre>
 <?php
-	//scrapes all of the development information from the massive dev chart
+//scrapes all of the development information from the massive dev chart
+	
+	$debug=false; //enables a little bit of extra variable output to help debug
+	$linebreak="\n"; //seems to need '\n' on windows while 'PHP_EOL' works on linux
+	
 	
 	$films = file_get_contents("http://www.digitaltruth.com/devchart.php");//get list of flims
 	$films = explode('<option value="">All Films</option>', trim($films));//get the list of films by itself
 	$films = preg_replace("/.+\"(.+)\".+/","$1",$films[1]);//get the film name selector value
-	$films = array_filter(explode(PHP_EOL, $films));//remove empty lines from the array
+	$films = array_filter(explode($linebreak, $films));//remove empty lines from the array
+	
+	if($debug){echo "<hr>List of films scraped<hr>\n"; print_r($films);}
 	
 	$fp = fopen('file.csv', 'w');//open the output file	
 	fputcsv($fp, array("Film","Developer","Dilution","ASA/ISO","35mm","120","Sheet","Temp","Notes")); //add header
@@ -29,10 +35,11 @@
 	    
 	    $data = trim($data);//get rid of white space
 	    
-	    $data = explode(PHP_EOL, $data);//split the data up by linebreaks
+	    $data = explode($linebreak, $data);//split the data up by linebreaks
 	    array_shift($data);//get rid of the header
 	    
 	    foreach ($data as &$value) {//loop through each item in the array of flim/dev combos
+			
 	        $film = explode("~", $value);//explode by the delimiter
 	        for ($i = 3; $i <= 6; $i++) {//some items in the massive dev chart have a range of 6-8. i'm averaging all of these, so 6-8 would just turn into 7.
 	            if (!is_numeric($film[$i]) && !strpos($film[$i], "+") && strpos($film[$i], "-")) {//check to see if the data has a hyphen.
@@ -42,6 +49,8 @@
 	        }
 	        fputcsv($fp, $film);// output that line to the output CSV
 	        echo $counter ." -- " . $film[0] . " -- " . $film[1] . "<br>"; //output some text as a progress indicator
+			if($debug){echo "Raw film information: $value<br>\n";
+						echo "Processed film information:"; print_r($film); echo "<br>\n<hr>\n";}
 			$counter++;
 	    }
 	}
